638e625de5c8008da69becacaac46d85e03df34f
Manish Gupta <manish@spotdraft.com>
2018-02-15 13:37:16 +0530

    Added Class For AccessToken, Booking & BookingProduct

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nexudus-python-api
+
+Usage:
+
+nexudus = AccessToken('username', 'password')
+api_call = nexudus.update_access_token(
+    Id=1,
+    BusinessId=1243,
+    AccessCode='1234',
+    MinutesIncluded=20,
+    MinutesLeft=0)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nexudus',
+      version='0.1',
+      description='Nexudus API Python Implementation',
+      url='https://github.com/manishgupta24/nexudus-python-api',
+      author='Manish Gupta',
+      author_email='manishgupta.ait@gmail.com',
+      license='GNU GPL',
+      packages=['nexudus'],
+      zip_safe=False)
